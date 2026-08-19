48b80dc9a07b86cffc23090877aa0463dbcf9437
jorge-flp <jorgefeliperodrigues841@gmail.com>
2026-08-19 09:47:38 -0300

    Criando projeto

---
+++ src/test/java/com/App_Escola/Api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.App_Escola.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
